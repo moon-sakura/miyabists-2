8d1b24441760da582c91178240f3d4be81268d39
wuxuan <skrumxuanw@gmail.com>
2026-03-27 22:10:32 +0800

    添加咏叹华彩

---
+++ Scripts/Cards/YongtanHuacai.cs
@@ -0,0 +1,50 @@
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using Miyabists2.Scripts.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miyabists2.Scripts.Cards
+{
+    internal class YongtanHuacai : MiyabiPartnerCardBase
+    {
+        public YongtanHuacai() : base(3, CardRarity.Rare, TargetType.Self, CardType.Power) { }
+
+        protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+        {
+            await PowerCmd.Apply<YongtanHuacaiPower>(Owner.Creature, 1, base.Owner.Creature, this);
+
+            if (base.CheckSupportCost(2) != 0)
+            {
+                CardSelectorPrefs prefs = new CardSelectorPrefs(base.SelectionScreenPrompt, 1);
+                List<CardModel> cardsIn = (from c in PileType.Draw.GetPile(base.Owner).Cards
+                                           where c.CanonicalKeywords.Contains(MiyabiKeywords.Friends)
+                                           orderby c.Rarity, c.Id
+                                           select c).ToList();
+                if (cardsIn.Count != 0)
+                {
+                    CardModel cardModel = (await CardSelectCmd.FromSimpleGrid(choiceContext, cardsIn, base.Owner, prefs)).FirstOrDefault();
+                    if (cardModel != null)
+                    {
+                        await CardPileCmd.Add(cardModel, PileType.Hand);
+                    }
+                }
+                await CostSupporPoint(2);
+            }
+        }
+
+
+        protected override void OnUpgrade()
+        {
+            EnergyCost.UpgradeBy(-1);
+        }
+    }
+}
---
+++ Scripts/Powers/YongtanHuacaiPower.cs
@@ -0,0 +1,34 @@
+using BaseLib.Abstracts;
+using Godot;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miyabists2.Scripts.Powers
+{
+    internal class YongtanHuacaiPower : CustomPowerModel
+    {
+        public override PowerType Type => PowerType.Buff;
+        public override PowerStackType StackType => PowerStackType.Counter;
+        public override Color AmountLabelColor => PowerModel._normalAmountLabelColor;
+        public string BigIconPath => "res://images/powers/commonPowers.png";
+        public string BigBetaIconPath => BigIconPath;
+        public override string CustomPackedIconPath => BigIconPath;
+        public override string CustomBigIconPath => BigIconPath;
+
+        public override async Task AfterCardPlayed(PlayerChoiceContext context, CardPlay cardPlay)
+        {
+            if(base.Owner == cardPlay.Card.Owner.Creature && cardPlay.Card.CanonicalKeywords.Contains(MiyabiKeywords.Friends))
+            {
+                await PlayerCmd.GainEnergy(Amount, base.Owner.Player);
+            }
+        }
+    }
+}
