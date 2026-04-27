411d93ca26ee4ba33cbaaad3441e07df29c15999
YueSakura <YueSakura@DESKTOP-QA5CNH0>
2026-04-27 09:15:59 +0800

    添加一切为了虎破王事件、添加了超越绝境之志遗物

---
+++ Scripts/Events/AllForHupowangEvent.cs
@@ -0,0 +1,84 @@
+using BaseLib.Abstracts;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Events;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.Rewards;
+using MegaCrit.Sts2.Core.Runs;
+using MegaCrit.Sts2.Core.ValueProps;
+using Miyabists2.Scripts.Cards;
+using Miyabists2.Scripts.Char;
+using Miyabists2.Scripts.Relics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miyabists2.Scripts.Events
+{
+    internal class AllForHupowangEvent : CustomEventModel
+    {
+        // 背景图位置
+        public override string? CustomInitialPortraitPath => "res://images/events/juFufu.png";
+
+        // 设置一些数值
+        protected override IEnumerable<DynamicVar> CanonicalVars =>
+        [
+            //new DamageVar(5m, ValueProp.Unblockable&ValueProp.Unpowered),
+            //new StringVar("Relic", ModelDb.Relic<SanluoXingdianRelic>().Title.GetFormattedText()),
+        ];
+
+        // 什么时候会遇到。
+        public override bool IsAllowed(IRunState runState)
+        {
+            return runState.Players.All(p => p.Character is Miyabi);
+        }
+
+        // 事件开始前的逻辑。
+        protected override Task BeforeEventStarted(bool isPreFinished)
+        {
+            return Task.CompletedTask;
+        }
+
+        // 事件结束后的逻辑。
+        protected override void OnEventFinished()
+        {
+
+        }
+
+        // 生成事件初始选项。
+        protected override IReadOnlyList<EventOption> GenerateInitialOptions() =>
+        [
+            Option(AllForHupowangChoice),
+            Option(Others),
+        ];
+
+        private async Task AllForHupowangChoice()
+        {
+            CardModel card = base.Owner.RunState.CreateCard<AllForHupowang>(base.Owner);
+            CardCmd.PreviewCardPileAdd(await CardPileCmd.Add(card, PileType.Deck), 2f);
+            SetEventFinished(PageDescription("JUFUFU"));
+        }
+
+        private async Task Others()
+        {
+            await RewardsCmd.OfferCustom(Owner!, [new CardReward(CardCreationOptions.ForNonCombatWithDefaultOdds([Owner!.Character.CardPool], GetBreakCards), 3, Owner)]);
+            SetEventFinished(PageDescription("OTHER_BREAKER"));
+        }
+
+        private bool GetBreakCards(CardModel card) 
+        {
+            if(card is ZuihuaYueyunzhuan) { return true; }
+            if(card is SongKe) { return true; }
+            if(card is CuteFeitianzhuang) { return true; }
+            if(card is CiquanLianji) { return true; }
+            if(card is MingfuWange) { return true; }
+            return false;
+        }
+
+    }
+}
---
+++ Scripts/Relics/ChaoyueJuejingZhizhiRelic.cs
@@ -0,0 +1,53 @@
+
+using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Rooms;
+using MegaCrit.Sts2.Core.Saves.Runs;
+using MegaCrit.Sts2.Core.ValueProps;
+using Miyabists2.Scripts.Cards;
+using Miyabists2.Scripts.Char;
+using Miyabists2.Scripts.Powers;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Miyabists2.Scripts.Relics
+{
+    [Pool(typeof(MiyabiRelicPool))]
+    internal class ChaoyueJuejingZhizhiRelic : CustomRelicModel
+    {
+        public override RelicRarity Rarity => RelicRarity.Common;
+        public override string PackedIconPath => "res://images/relics/chaoyueJuejing.png";
+        protected override string PackedIconOutlinePath => PackedIconPath;
+        protected override string BigIconPath => PackedIconPath;
+
+        protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+            // HoverTipFactory.FromCard<MyCard>(),
+        ];
+
+        public override decimal ModifyDamageMultiplicative(Creature? target, decimal amount, ValueProp props, Creature? dealer, CardModel? cardSource)
+        {
+            if (dealer != base.Owner.Creature)
+            {
+                return 1m;
+            }
+            bool isValidMove = props.HasFlag(ValueProp.Move) && !props.HasFlag(ValueProp.Unpowered);
+            if (!isValidMove) return 1m;
+
+            decimal percent = base.Owner.Creature.CurrentHp / base.Owner.Creature.MaxHp;
+            if (percent >= 0.8m) return 0.5m;
+            if (percent <= 0.2m) return 1.5m;
+            if (percent <= 0.5m) return 1.2m;
+            return 1m;
+        }
+
+    }
+}
