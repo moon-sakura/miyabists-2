31e27047b8247da6abf45ee364ba6af11c3bffa3
YueSakura <YueSakura@DESKTOP-QA5CNH0>
2026-06-27 14:59:35 +0800

    添加模组敌人出现选项

--- Scripts/Service/MiyabiModConfig.cs
+++ Scripts/Service/MiyabiModConfig.cs
@@ -113,6 +113,8 @@ namespace Miyabists2.Scripts.Service
         //[ConfigHoverTip]
         public static bool MiyabiEnemiesStronger { get; set; } = false;
 
+        public static bool MiyabiEnemiesAppearWhenPlayingOtherChar { get; set; } = true;
+
         //[ConfigSlider(0.5, 5.0, 0.1, Format = "{0:0.#}x")]
         //[ConfigHoverTip]
         //public static double MonsterHpMax { get; set; } = 1.0;
